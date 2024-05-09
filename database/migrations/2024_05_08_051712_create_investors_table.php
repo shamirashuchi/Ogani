c4d58eb1d8084f193e4d36fcf35fc820d62e042f
shamirashuchi <shamirashuchi@gmail.com>
2024-05-09 09:05:14 +0600

    erp start

---
+++ database/migrations/2024_05_08_051712_create_investors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('investors', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->decimal('investment_amount', 10, 2);
+            $table->tinyInteger('flag')->default(0);
+            $table->string('action')->default('Requested');
+            $table->datetime('custom_created_at')->nullable();
+            $table->datetime('custom_updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('investors');
+    }
+};
